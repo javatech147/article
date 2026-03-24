699dcfa9f13653ed28266514432bb1205f261789
javatech147 <javatech147@gmail.com>
2026-03-24 12:19:06 +0530

    Added toolbar

--- app/src/main/java/com/example/articlemanager/presentation/MainActivity.kt
+++ app/src/main/java/com/example/articlemanager/presentation/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
 import com.example.articlemanager.di.AppContainer
 import com.example.articlemanager.presentation.theme.ArticleManagerTheme
@@ -16,12 +21,24 @@ class MainActivity : ComponentActivity() {
         AppContainer(applicationContext)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ArticleManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Article Manager") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary
+                            ),
+                        )
+                    }
+                ) { innerPadding ->
                     AppNavGraph(
                         appContainer = appContainer,
                         modifier = Modifier.padding(innerPadding)
